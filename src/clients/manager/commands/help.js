115d463086d0ee98bebd7f055714e47cebe5c1ed
Xynnix <zkjcksn@gmail.com>
2023-09-27 21:21:14 -0400

    Oops

---
+++ src/clients/manager/commands/help.js
@@ -0,0 +1,38 @@
+const ms = require("ms");
+const reactions = ["🔔"];
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  category: "Info",
+  description: "Commands list",
+  async run(client, message, args) {
+    try {
+      const embed = {
+        "title": msg.author.username,
+        "colour": msg.member.hoistedRole.colour,
+        "icon_url": msg.author.generateAvatarURL({ size: 4096, }, true),
+        "description": `| Command | Example | Description |\n|----------|----------|----------|\n| Vote | {prefix} vote <@Bot>  | vote for a bot on the list   |\n| bot   | {prefix} info <@Bot>   | shows bot information |\n| user   | {prefix} user <@user>   | shows user information |\n| help   | {prefix} help | shows this response. |\n| ping   | {prefix} ping | shows my latency information. |`
+      }
+      return message.reply({ embeds: [embed] })
+
+    } catch (err) {
+      console.error(err);
+      message.channel.stopTyping();
+      await message.reply({
+        embeds: [
+          {
+            description: `:x: There was an error while executing this command! \n\`\`\`js\n${err}\`\`\``,
+            colour: "#ff4654",
+          },
+        ],
+      });
+    }
+  },
+};
+
+function canUserVote(x) {
+  const left = x.time - (Date.now() - x.date),
+    formatted = ms(left, { long: true });
+  if (left <= 0 || formatted.includes("-")) return { status: true };
+  return { status: false, left, formatted };
+}
---
+++ src/clients/manager/commands/user.js
@@ -0,0 +1,67 @@
+const ms = require("ms");
+const reactions = ["🔔"];
+module.exports = {
+  name: "user",
+  aliases: ["ui", "userinfo"],
+  category: "BotList",
+  description: "Check for a user on the botlist.",
+  async run(client, message, args) {
+    try {
+      let userModel = require("../../../database/models/user");
+
+      let BotRaw;
+      if (new RegExp(`(<@!?(.*)>)`).test(args[0])) {
+        BotRaw = await client.users.fetch(
+          args[0].match(new RegExp(`(<@!?(.*)>)`))[2]
+        );
+      } else {
+        BotRaw = await client.users.fetch(args[0]).catch(() => {
+          BotRaw = false;
+        });
+      }
+      if (!BotRaw || BotRaw.bot)
+        return message.reply("This is not a real user. :hmm:");
+
+        let x = await userModel.findOne({
+        user: BotRaw.user._id,
+      });
+      let y = await userModel.filter(f=>{
+        f.owners.includes(BotRaw.user._id)
+      });
+      
+      let bots;
+      if (y){
+       await y.forEach(b => {
+          bots.push([b.id,b.name, b.verified, b.certified])
+        });
+      }
+      console.log(y)
+      if (x) {
+          const embed = {title: `${BotRaw.user.name}`, description:`Bio: "__${x.bio}__"\n\nDescription: ${x.description}
+          | Github | Website | X | RBL |
+          |----|----|----|----|
+          |[GitHub](${x.github||"N/A"}) | [Website](${x.website||"N/A"})| [X](${x.twitter||"N/A"})|[RBL](https://revoltbots.org/users/${x.revoltId})|
+          `,icon_url:BotRaw.user.generateAvatarURL({size: 4096}, true)}
+
+          const embed2 = {title: `${BotRaw.user.name}`, description:`Users Bots: ${bots?.length}
+          | ID | Name | Verified | URL |
+          |----|----|----|----|
+          |[GitHub](${x.github||"N/A"}) | [Website](${x.website||"N/A"})| [X](${x.twitter||"N/A"})|[RBL](https://revoltbots.org/users/${x.revoltId})|
+          `,icon_url:BotRaw.user.generateAvatarURL({size: 4096}, true)}
+          message.reply({embeds:[embed]})
+
+      }
+    } catch (err) {
+      console.error(err);
+      message.channel.stopTyping();
+      await message.reply({
+        embeds: [
+          {
+            description: `:x: There was an error while executing this command! \n\`\`\`js\n${err}\`\`\``,
+            colour: "#ff4654",
+          },
+        ],
+      });
+    }
+  },
+};
