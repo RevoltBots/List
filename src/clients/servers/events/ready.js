d4a9bea17d6e1d3465250a7f8eed0796c4c854fd
NightRaven <53287573+Xynnix@users.noreply.github.com>
2024-02-02 13:39:19 -0500

    Update ready.js

--- src/clients/servers/events/ready.js
+++ src/clients/servers/events/ready.js
@@ -1,9 +1,11 @@
 
 module.exports = {
   name: "ready",
+  once: true,
   async execute(client) {
     console.info(`[INFO] ${client.user.username} is logged in and ready.`);
     global.ServerClient = client;
+    
 /*    client.users.edit({
       status: {
         text: "Watching de serverlist",
