206396e7a52da079b4e4604aaa7ebc29ea20a658
NightRaven <53287573+Xynnix@users.noreply.github.com>
2024-09-07 03:06:57 -0400

    Update users.js

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -85,7 +85,7 @@ router.get("/:id/edit", checkAuth, async (req, res) => {
 })
 
 router.post("/:id/edit", checkAuth, async (req, res) => {
-  if (req.params.id !== req.session.userAccountId) return res.render("error.ejs", { message: "You are not authorized to perform this action." } });
+  if (req.params.id !== req.session.userAccountId) return res.render("error.ejs", { message: "You are not authorized to perform this action." });
   const data = req.body;
   if (!data) return res.redirect(`#?message=No+data+was+changed+within+your+profile!`);
 
